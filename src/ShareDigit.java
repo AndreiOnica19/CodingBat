57d7823a147a72542822843d31b28b27b84e9447
AndreiOnica <onica.andrei19@yahoo.com>
2024-05-13 20:23:31 +0300

    Added solution

--- src/ShareDigit.java
+++ src/ShareDigit.java
@@ -1,6 +1,7 @@
 public class ShareDigit {
     public static void main(String[] args) {
-
+        shareDigit(12,43);
+        System.out.println(shareDigit(12,43));
     }
 
 
@@ -11,7 +12,13 @@ public class ShareDigit {
     public static boolean shareDigit(int a, int b) {
         String txtA = String.valueOf(a);
         String txtB = String.valueOf(b);
-
+        for (int i = 0; i < txtA.length(); i++) {
+            for (int j = 0; j < txtB.length(); j++) {
+                if (txtA.charAt(i) == txtB.charAt(j)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-
 }
