80a8b997688e67aeb8f32cf988a553d855905aee
AndreiOnica <onica.andrei19@yahoo.com>
2024-05-13 19:58:09 +0300

    Initial commit

---
+++ src/ShareDigit.java
@@ -0,0 +1,17 @@
+public class ShareDigit {
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+    * Given two ints, each in the range 10..99, return true if there is a digit that appears in both numbers,
+    * such as the 2 in 12 and 23. (Note: division, e.g. n/10, gives the left digit while the % "mod" n%10 gives the right digit.)
+     */
+    public static boolean shareDigit(int a, int b) {
+        String txtA = String.valueOf(a);
+        String txtB = String.valueOf(b);
+
+    }
+
+}
