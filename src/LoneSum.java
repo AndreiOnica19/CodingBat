bf2a969c279659a829035a30886fd2def872f71a
AndreiOnica <onica.andrei19@yahoo.com>
2024-05-15 18:06:18 +0300

    Change comment position

--- src/LoneSum.java
+++ src/LoneSum.java
@@ -1,10 +1,11 @@
 public class LoneSum {
     public static void main(String[] args) {
-        /** Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values,
-         it does not count towards the sum.
-         */
         System.out.println(loneSum(12, 13, 15));
     }
+
+    /** Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values,
+     it does not count towards the sum.
+     */
 
     public static int loneSum(int a, int b, int c) {
         int sum = a + b + c;
