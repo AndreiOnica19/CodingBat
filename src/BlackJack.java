d327fe4329169016ef73096079ecf964f5eff688
AndreiOnica <onica.andrei19@yahoo.com>
2024-05-16 18:34:08 +0300

    small changes

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,26 +1,23 @@
 public class BlackJack {
     public static void main(String[] args) {
-        System.out.println(blackjack(23,23));
+        System.out.println(blackjack(23, 23));
     }
 
-    /**Given 2 int values greater than 0, return whichever value is nearest to 21 without going over.
+    /**
+     * Given 2 int values greater than 0, return whichever value is nearest to 21 without going over.
      * Return 0 if they both go over.
      */
 
     public static int blackjack(int a, int b) {
-        if(a > 21 && b > 21){
+        if (a > 21 && b > 21) {
             return 0;
-        }
-        if(a > 21){
+        } else if (a > 21) {
             return b;
-        }
-        if(b > 21){
+        } else if (b > 21) {
             return a;
-        }
-        if(a > b){
+        } else if (a > b) {
             return a;
-        }
-        else {
+        } else {
             return b;
         }
     }
